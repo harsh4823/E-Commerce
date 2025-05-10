harsh4823/spring_basics

Product Structure Added

src/main/java/com/basics/spring_basics/Model/CategoryModel.java
@@ -1,14 +1,13 @@
 package com.basics.spring_basics.Model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Entity(name = "categories")
 @Data
@@ -22,4 +21,7 @@ public class CategoryModel {
     @NotBlank(message = "name cant be blank")
     @Size(min = 5,message = "name should contain at least 5 letters")
     private String categoryName;
+
+    @OneToMany(mappedBy = "category",cascade = CascadeType.ALL)
+    private List<Products> products;
 }

src/main/java/com/basics/spring_basics/Repository/ProductRepository.java
@@ -1,0 +1,18 @@
+package com.basics.spring_basics.Repository;
+
+import com.basics.spring_basics.Model.CategoryModel;
+import com.basics.spring_basics.Model.Products;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Products,Long> {
+
+    Products findByproductName(String productName);
+
+    Page<Products> findByproductNameLikeIgnoreCase(Pageable pageDetails, String keyword);
+
+    Page<Products> findByCategoryOrderByPriceAsc(Pageable pageDetails, CategoryModel categoryId);
+}

src/main/java/com/basics/spring_basics/Service/FileServiceImp.java
@@ -1,0 +1,56 @@
+package com.basics.spring_basics.Service;
+
+import com.basics.spring_basics.Exceptions.ResourceNotFoundException;
+import com.basics.spring_basics.Model.Products;
+import com.basics.spring_basics.Payload.ProductsDTO;
+import com.basics.spring_basics.Repository.ProductRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.UUID;
+
+@Service
+public class FileServiceImp implements FileService{
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Value("${project.image}")
+    private String path;
+
+    @Override
+    public ProductsDTO updateProductImage(Long productID, MultipartFile image) throws IOException {
+        Products product = productRepository.findById(productID)
+                .orElseThrow(()->new ResourceNotFoundException("Product","ProductID",productID));
+        String fileName = uploadImage(path,image);
+        product.setImage(fileName);
+        return modelMapper.map(productRepository.save(product), ProductsDTO.class);
+    }
+
+    private String uploadImage(String path, MultipartFile image) throws IOException {
+
+        String originalFileName = image.getOriginalFilename();
+
+        String randomID = UUID.randomUUID().toString();
+        String fileExtension = randomID.concat(originalFileName.substring(originalFileName.lastIndexOf('.')));
+        String fileName = randomID + fileExtension;
+
+        File folder = new File(path);
+        if (!folder.exists()){
+            folder.mkdirs();
+        }
+        String filePath = path + File.separator + fileName;
+        Files.copy(image.getInputStream(), Path.of(filePath));
+        return fileName;
+    }
+}

harsh4823/E-Commerce

src/main/java/com/basics/spring_basics/Model/CategoryModel.java
@@ -1,14 +1,13 @@
 package com.basics.spring_basics.Model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Entity(name = "categories")
 @Data
@@ -22,4 +21,7 @@ public class CategoryModel {
     @NotBlank(message = "name cant be blank")
     @Size(min = 5,message = "name should contain at least 5 letters")
     private String categoryName;
+
+    @OneToMany(mappedBy = "category",cascade = CascadeType.ALL)
+    private List<Product> products;
 }

src/main/java/com/basics/spring_basics/Repository/ProductRepository.java
@@ -1,0 +1,18 @@
+package com.basics.spring_basics.Repository;
+
+import com.basics.spring_basics.Model.CategoryModel;
+import com.basics.spring_basics.Model.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product,Long> {
+
+    Product findByproductName(String productName);
+
+    Page<Product> findByproductNameLikeIgnoreCase(Pageable pageDetails, String keyword);
+
+    Page<Product> findByCategoryOrderByPriceAsc(Pageable pageDetails, CategoryModel categoryId);
+}

src/main/java/com/basics/spring_basics/Service/FileServiceImp.java
@@ -1,0 +1,56 @@
+package com.basics.spring_basics.Service;
+
+import com.basics.spring_basics.Exceptions.ResourceNotFoundException;
+import com.basics.spring_basics.Model.Product;
+import com.basics.spring_basics.Payload.ProductsDTO;
+import com.basics.spring_basics.Repository.ProductRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.UUID;
+
+@Service
+public class FileServiceImp implements FileService{
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Value("${project.image}")
+    private String path;
+
+    @Override
+    public ProductsDTO updateProductImage(Long productID, MultipartFile image) throws IOException {
+        Product product = productRepository.findById(productID)
+                .orElseThrow(()->new ResourceNotFoundException("Product","ProductID",productID));
+        String fileName = uploadImage(path,image);
+        product.setImage(fileName);
+        return modelMapper.map(productRepository.save(product), ProductsDTO.class);
+    }
+
+    private String uploadImage(String path, MultipartFile image) throws IOException {
+
+        String originalFileName = image.getOriginalFilename();
+
+        String randomID = UUID.randomUUID().toString();
+        String fileName = randomID.concat(originalFileName.substring(originalFileName.lastIndexOf('.')));
+
+
+        File folder = new File(path);
+        if (!folder.exists()){
+            folder.mkdirs();
+        }
+        String filePath = path + File.separator + fileName;
+        Files.copy(image.getInputStream(), Path.of(filePath));
+        return fileName;
+    }
+}