50ab6aac940aab5dacec5af32d2199464c2e9724
harsh <harsh924823@gmail.com>
2025-07-10 02:27:55 +0530

    built productCard component

--- frontend/src/Components/Products.jsx
+++ frontend/src/Components/Products.jsx
@@ -3,14 +3,14 @@ import {FaExclamationTriangle} from "react-icons/fa";
 
 const Products = () => {
     const isLoading = false;
-    const errorMessage = "An error occurred";
-   const products = [
+    const errorMessage = "";
+    const products = [
         {
             productId: 652,
             productName: "Iphone Xs max",
             image: "https://placehold.co/600x400",
             description: "Experience the latest in mobile technology with advanced cameras, powerful processing, and an all-day battery.",
-            quantity: 0,
+            quantity: 10,
             price: 1450.0,
             discount: 10.0,
             specialPrice: 1305.0,
