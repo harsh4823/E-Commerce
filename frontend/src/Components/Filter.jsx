1c9e0b77153eb7cc20cb38335a428e43a6b76288
harsh <harsh924823@gmail.com>
2025-07-22 17:28:25 +0530

    given life to filter buttons

--- frontend/src/Components/Filter.jsx
+++ frontend/src/Components/Filter.jsx
@@ -1,7 +1,8 @@
-import {useState} from "react";
-import {FiArrowUp, FiRefreshCcw, FiRefreshCw, FiSearch} from "react-icons/fi";
+import {useEffect, useState} from "react";
+import {FiArrowDown, FiArrowUp, FiRefreshCcw, FiRefreshCw, FiSearch} from "react-icons/fi";
 import {Button, MenuItem, Select, Tooltip} from "@mui/material";
 import {FormControl, InputLabel} from "@mui/material";
+import {useLocation, useNavigate, useSearchParams} from "react-router-dom";
 
 const Filter = () => {
 
@@ -13,10 +14,68 @@ const Filter = () => {
         { categoryId: 5, categoryName: 'Sports' }
     ];
 
-    const [category,setCategory] = useState("all");
+    const [searchParams] = useSearchParams();
+    const URLParams = new URLSearchParams(searchParams);
+    const path = useLocation().pathname;
+    const navigate = useNavigate();
 
-    const handleChangeCategory = (event) => {
+    const [category,setCategory] = useState("all");
+    const [sortOrder,setSortOrder] = useState("asc");
+    const [searchTerm,setSearchTerm] = useState("");
+
+    useEffect(()=>{
+        const currentCategory = searchParams.get('category') || 'all';
+        const currentSortOrder = searchParams.get('sortBy') || 'asc';
+        const currentSearchTerm = searchParams.get('keyword') || '';
+
+        setCategory(currentCategory);
+        setSortOrder(currentSortOrder);
+        setSearchTerm(currentSearchTerm);
+    },[searchParams]);
+
+    useEffect(()=>{
+
+        const handler = setTimeout(()=>{
+            if (searchTerm){
+                searchParams.set('keyword',searchTerm);
+            }else{
+                searchParams.delete('keyword');
+            }
+            navigate(`${path}?${searchParams.toString()}`);
+        },700);
+
+        return()=>{
+            clearTimeout(handler);
+        }
+
+    },[searchParams,searchTerm,navigate,path]);
+
+    const handleCategoryChange = (event) => {
+        const selectedCategory = event.target.value;
+
+        if(selectedCategory==='all'){
+            searchParams.delete('category');
+        }else{
+            searchParams.set('category',selectedCategory);
+        }
+        navigate(`${path}?${searchParams}`)
         setCategory(event.target.value);
+    }
+
+    const toggleSortOrder = () => {
+        setSortOrder((prevOrder) => {
+            const newOrder = (prevOrder==='asc') ? 'desc' : 'asc';
+            searchParams.set('sortBy',newOrder);
+            navigate(`${path}?${searchParams}`);
+            return newOrder;
+        });
+    }
+
+    const handleClearFilter= () => {
+        navigate({pathname : window.location.pathname});
+        setCategory('all');
+        setSortOrder('asc');
+        setSearchTerm('');
     }
 
     return(
@@ -26,7 +85,8 @@ const Filter = () => {
             <div className={'relative flex items-center 2xl:w-[450px] sm:w-[420px] w-full'}>
                 <input type="text"
                        className={'border border-gray-400 text-slate-800 w-full rounded-md py-2 pr-4 pl-10 focus:outline-none focus:ring-2 focus:ring-[#1976d3]'}
-                       placeholder={'Search Products'}/>
+                       placeholder={'Search Products'}
+                value={searchTerm} onChange={(e)=>setSearchTerm(e.target.value)}/>
                 <FiSearch className={'absolute left-3 text-slate-800 size-{20}'}/>
             </div>
 
@@ -36,12 +96,15 @@ const Filter = () => {
                 variant={'outlined'} size={'small'}>
                     <InputLabel id={'category-select-label'}>Category</InputLabel>
                     <Select variant={'outlined'} labelId={'category-select-label'}
-                            value={category} onChange={handleChangeCategory} label='Category'
-                    className={'min-w-[120px] text-slate-800 border-slate-700'}>
-                    <MenuItem value={'All'}>All</MenuItem>
+                             onChange={handleCategoryChange} label='Category'
+                    className={'min-w-[120px] text-slate-800 border-slate-700'}
+                            value={searchParams.get('category') || 'all'}>
+                    <MenuItem value={'all'} >All</MenuItem>
                     {categories.map((category)=>{
                         return(
-                            <MenuItem value={category.categoryName} key={category.categoryId}>{category.categoryName}</MenuItem>
+                            <MenuItem value={category.categoryName} key={category.categoryId}>
+                                {category.categoryName}
+                            </MenuItem>
                         )
                     })}
                     </Select>
@@ -49,14 +112,21 @@ const Filter = () => {
 
                 {/*Sort Button and Clear Button*/}
                 <Tooltip title={'Sorted By Price : asc'} children={''}>
-                    <Button variant={'contained'} color={'primary'} className={'flex items-center gap-2 h-10'}>
+                    <Button variant={'contained'} color={'primary'} className={'flex items-center gap-2 h-10'}
+                            onClick={toggleSortOrder}>
                         Sort By
-                        <FiArrowUp size={20}/>
+                        {sortOrder==='asc' ? (
+                            <FiArrowUp size={20}/>
+                        ): (
+                            <FiArrowDown size={20}/>
+                        )
+                        }
                     </Button>
                 </Tooltip>
                 <button className={'flex items-center justify-center gap-2 bg-rose-900 ' +
                     'text-white px-3 py-2 cursor-pointer rounded-md transition duration-300 ease-in ' +
-                    'shadow-md focus:outline-none'}>
+                    'shadow-md focus:outline-none'}
+                    onClick={handleClearFilter}>
                     <FiRefreshCw className={'font-semibold'} size={16}/>
                    <span className={'font-semibold'}>Clear Filter</span>
                 </button>
