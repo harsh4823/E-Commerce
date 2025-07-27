c881addaf7f5960e79356af0b7e0f178a877b436
harsh <harsh924823@gmail.com>
2025-07-27 20:12:36 +0530

    created navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import Products from "./Components/Products/Products.jsx";
 import {BrowserRouter as Router,Routes,Route } from "react-router-dom";
 import Home from "./Components/Home/Home.jsx";
+import NavBar from "./Components/Shared/NavBar.jsx";
 
 function App() {
 
     return(
         <div>
           <Router>
+              <NavBar/>
               <Routes>
                   <Route path={'/'} element={<Home/>}/>
                   <Route path={'/products'} element={<Products/>}/>
