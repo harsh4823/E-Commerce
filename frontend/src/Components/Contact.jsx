89309e9f781812fd534af4f98cc65a8dfcffbe89
harsh4823 <harsh924823@gmail.com>
2025-07-29 00:58:17 +0530

    contact page created

--- frontend/src/Components/Contact.jsx
+++ frontend/src/Components/Contact.jsx
@@ -1,10 +1,9 @@
-import { FaEnvelope, FaMapMarkedAlt, FaPhone } from "react-icons/fa";
-
+import {FaEnvelope, FaMapMarkedAlt, FaPhone} from 'react-icons/fa'
 const Contact = () => {
     return(
         <div
             className="flex flex-col items-center justify-center min-h-screen py-12 bg-cover bg-center"
-            style={{backgroundImage: "url('')"}}>
+            style={{backgroundImage: "url('src/assets/images/bg.jpg')"}}>
             
             <div className="bg-white shadow-lg rounded-lg p-8 w-full max-w-lg">
                 <h1 className="text-4xl font-bold text-center mb-6">Contact us</h1>
@@ -43,7 +42,34 @@ const Contact = () => {
                             required
                             className="mt-1 block w-full border border-gray-300 rounded-lg p-2 focus:outline-hidden focus:ring-2 focus: ring-blue-500"/>
                     </div>
+                    <button className="w-full bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600 transition duration-300">
+                        Send Message
+                    </button>
                     </form>
+
+                    <div className="mt-8 text-center">
+                        <h2 className="text-lg font-semibold">
+                            Contact Information
+                        </h2>
+
+                        <div className="flex flex-col items-center space-y-2 mt-4 ">
+                            <div className="flex items-center">
+                                <FaPhone className='text-blue-500 mr-2'/>
+                                <span className='text-slate-600'>+91 1234567890</span>
+                            </div>
+
+
+                            <div className="flex items-center">
+                                <FaEnvelope className='text-blue-500 mr-2'/>
+                                <span className='text-slate-600'>harsh924823@gmail.com</span>
+                            </div>
+
+                            <div className="flex items-center">
+                                <FaMapMarkedAlt className='text-blue-500 mr-2'/>
+                                <span className='text-slate-600'>XXXXXX</span>
+                            </div>
+                        </div>
+                    </div>
                     </div>
                 </div>
     );
