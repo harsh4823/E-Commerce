22272a44d5e2a617a5e78ff5f2a8372e66417332
harsh4823 <harsh924823@gmail.com>
2025-07-29 17:19:29 +0530

    building cart functionality

---
+++ frontend/src/store/action/cartAction.js
@@ -0,0 +1,27 @@
+export const addtoCart = (data,qty=1) =>
+     (dispatch,getState) => {
+        // Find Product 
+        const {products} = getState.products;
+        const getProduct = products.find(
+            (item) => item.productId === data.productId
+        );
+
+        // Check for Stocks
+        const isQuantityExists = getProduct.quantity >= qty;
+
+        // If in Stock -> add
+        if(isQuantityExists){
+            dispatch(
+                {
+                    type : "Add_To_Cart",
+                    payload : {
+                        ...data,
+                        quantity:qty,
+                    }
+                }
+            );
+            localStorage.setItem("cartItems",JSON.stringify(getState().carts.cart));
+        }else{
+            // error
+        }
+}
--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -3,14 +3,23 @@ import {productReducer} from "./reducers/ProductReducer.js";
 import {errorReducer} from "./reducers/ErrorReducer.js";
 import {categoryReducer} from "./reducers/CategoryReducer.js";
 
+const cartItems = localStorage.getItem("cartItems")?
+        JSON.parse(localStorage.getItem("cartItems")):
+        [];
+
+const initialState = {
+    carts : cartItems,
+};
+
 export const store = configureStore(
     {
         reducer:{
             products : productReducer,
             errors : errorReducer,
             categories : categoryReducer,
+            carts : categoryReducer,
         },
-        preloadedState:{},
+        preloadedState : initialState,
     }
 );
 
