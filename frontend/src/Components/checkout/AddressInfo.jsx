c6ad35de13eb2b38851298089d71f080ffcba7ec
harsh4823 <harsh924823@gmail.com>
2025-08-07 21:51:36 +0530

    building address ui

--- frontend/src/Components/checkout/AddressInfo.jsx
+++ frontend/src/Components/checkout/AddressInfo.jsx
@@ -1,7 +1,38 @@
 import React from 'react'
+import Skeleton from '../Shared/Skeleton';
+import { FaAddressBook } from 'react-icons/fa';
 
 export const AddressInfo = () => {
+  const noAddressExit = true;
+  const isLoading = false;
   return (
-    <div>AddressInfo</div>
+    <div className='pt-4'>
+      {noAddressExit ? (
+        <div className='p-6 rounded-lg max-w-md mx-auto flex flex-col items-center justify-center'>
+          <FaAddressBook size={50} className='text-gray-500 mb-4' />
+           <h1 className='mb-2 text-slate-900 text-center font-semibold text-2xl'>
+              No Address Added Yet
+            </h1>
+           <p className='mb-6 text-slate-800 text-center'>
+              Please add your address to complete purchase
+            </p>
+          </div>
+      ): (
+          <div className='relative p-6 rounded-lg max-w-md mx-auto'>
+            <h1 className='text-slate-800 text-center font-bold text-2xl'>
+              Select Address
+            </h1>
+            
+            {isLoading ?
+              <div className='py-4 px-8'>
+                <Skeleton />
+              </div>:
+              <div className='space-y-4 pt-6'>
+                 <p>Address List</p> 
+              </div>
+          }
+          </div>
+      )}
+    </div>
   )
 }
