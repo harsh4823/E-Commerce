88bf7395d2f7cb08b3a705624c324c44439a7af5
harsh4823 <harsh924823@gmail.com>
2026-09-10 19:49:29 +0530

    changed import style

--- frontend/src/Utils/index.js
+++ frontend/src/Utils/index.js
@@ -1,23 +1,27 @@
 import { FaBoxOpen, FaHome, FaShoppingCart, FaStore, FaThList } from "react-icons/fa";
+import sofaImg from "../assets/images/Sofa.png";
+import tvImg from "../assets/images/TV.png";
+import kidsClothesImg from "../assets/images/KidsClothes.png";
+
 
 export const banner = [
     {
         id: 1,
-        image: "src/assets/images/Sofa.png",
+        image: sofaImg,
         title: "Home Comfort",
         subtitle: "Living Room",
         description: "Upgrade your space with cozy and stylish sofas",
     },
     {
         id: 2,
-        image: "src/assets/images/TV.png",
+        image: tvImg,
         title: "Entertainment Hub",
         subtitle: "Smart TV",
         description: "Experience the latest in home entertainment",
     },
     {
         id: 3,
-        image: "src/assets/images/KidsClothes.png",
+        image: kidsClothesImg,
         title: "Playful Picks",
         subtitle: "Kids' Clothing",
         description: "Bright and fun styles for kids, up to 20% off",
